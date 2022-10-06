ed7d8ff53ff69dec1e4f82cdadc08a456349c612
Juan Ahumada <juan.ahumada94@gmail.com>
2022-10-06 07:41:31 -0500

    Primeros Pasos con Kotlin

---
+++ app/src/main/java/com/example/baseskotlin/Principal.kt
@@ -0,0 +1,16 @@
+package com.example.baseskotlin
+
+fun main(){
+    newTopic("Primeros Pasos con Kotlin")
+}
+
+fun newTopic(topic: String){
+    /*print("==================== ")
+    print(topic)
+    print(" ==================== ")*/
+
+    /*println("===================="+ topic +"====================")*/
+
+    println("========== $topic ==========")
+}
+
