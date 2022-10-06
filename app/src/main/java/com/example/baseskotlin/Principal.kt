47d88ea541cde7a1c5a83ec23f95a5e9de844cda
Juan Ahumada <juan.ahumada94@gmail.com>
2022-10-06 07:55:04 -0500

    Variables y Constantes

--- app/src/main/java/com/example/baseskotlin/Principal.kt
+++ app/src/main/java/com/example/baseskotlin/Principal.kt
@@ -2,12 +2,25 @@ package com.example.baseskotlin
 
 fun main(){
     newTopic("Primeros Pasos con Kotlin")
+    print("==================== ")
+    print("Impresiones")
+    print(" ====================\n ")
+    newTopic("Variables y Constantes")
+    // val a = 7 Declaracion y asignacion sin tipo de dato
+    // a = 2 comprobamos que es una constante
+    val a: Int = 7 // Declaracion y asignacion de constante con su tipo de dato
+    // Declaracion de variable
+    var b = 14
+
+
+    println("El valor de la constante a es de: $a")
+    println("El valor de la variable b es de: $b")
+
+
 }
 
 fun newTopic(topic: String){
-    /*print("==================== ")
-    print(topic)
-    print(" ==================== ")*/
+
 
     /*println("===================="+ topic +"====================")*/
 
